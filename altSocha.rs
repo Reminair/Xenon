41c3e37723208c8c918f44cec0282fa435e04a19
Reminair <reminair@protonmail.com>
2025-01-13 02:11:18 -0500

    Update altSocha.rs

--- altSocha.rs
+++ altSocha.rs
@@ -1,12 +1,1 @@
-use std::{
-    env,
-    io,
-    fs,
-    path::PathBuf
-};
-use crate::cli;
 
-pub fn run() -> io::Result<()> {
-    let mut currentDir = env::current_dir();
-    println!("{}", currentDir);
-}
