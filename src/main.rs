8c6b0173bc62d88cbd7069f192a9b3c10aa2c7be
Reminair <reminair@protonmail.com>
2025-01-20 17:15:33 -0500

    Minor optimizations I migrated cli.rs to main.rs for better readability, I also made a seperate function for file reading called read_file(<file name + dir>), whenever the function is called it directly outputs the file contents to the terminal

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,29 @@
-use colored::Colorize;
 use std::{
     thread,
-    time
+    time,
+    fs::File,
+    path::Path,
+    io::{
+        stdout,
+        self,
+        Write,
+        BufRead
+    }
 };
+use crossterm::{
+    ExecutableCommand,
+    cursor::{
+        MoveToColumn,
+        MoveToRow
+    },
+    terminal::{
+        Clear,
+        ClearType
+    }
+};
+use colored::Colorize;
 
-mod cli;
+// Import local modules
 mod socha;
 mod sfetch;
 
@@ -12,7 +31,7 @@ pub static OS_NAME: &str = "Xenon 1.2";
 
 fn main() {
     boot();
-    cli::run();
+    cli();
 }
 
 fn boot() {
@@ -33,4 +52,80 @@ fn boot() {
     }
 
     println!();
+}
+
+pub fn clear() {
+    let mut stdout = stdout();
+    let _ = stdout.execute(MoveToColumn(0));            // Move to left-most column
+    let _ = stdout.execute(MoveToRow(0));               // Move to top-most row
+    let _ = stdout.execute(Clear(ClearType::All));      // Clear screen
+    let _ = stdout.execute(Clear(ClearType::Purge));    // Clear history
+}
+
+// Parse file info
+fn get_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where P: AsRef<Path>, {
+    let file = File::open(filename)?;       // Open file
+    Ok(io::BufReader::new(file).lines())    // Put file lines into a buffer?
+}
+
+// Output file info
+fn read_lines(file: &str) {
+    if let Ok(lines) = get_lines(file) {
+        // Consumes the iterator, returns an (Optional) String
+        for line in lines.map_while(Result::ok) {
+            println!("{}", line.bold().purple());
+        }
+    }
+}
+
+pub fn cli() {
+    loop {
+        // Display a prompt for the user
+        print!("{}>", "Xenon".bold().purple());
+        io::stdout().flush().unwrap();
+
+        // Read the user's input
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).unwrap();
+
+        // Remove the newline character from the input
+        let input = input.trim();
+
+        // Parse arguments
+        let mut input_pieces = input.split_whitespace();
+        let command = input_pieces.next().unwrap_or("");
+
+        // Execute the command
+        match command {
+            "help" => {
+                read_lines("help.txt");
+            }
+            "clr" => {
+                clear();
+            }
+            "say" => {
+                let text = input_pieces.collect::<Vec<&str>>().join(" ");
+
+                println!("{}", text);
+                io::stdout().flush().unwrap();
+            }
+            "sfetch" => {
+                sfetch::run();
+            }
+            "socha" => {
+                let _ = socha::run();
+            }
+            "credits" => {
+                read_lines("credits.txt");
+            }
+            "shutdwn" => {
+                println!("Goodbye!...");
+                std::process::exit(0);
+            }
+            _ => {
+                println!("Command not recognized. Type 'help' for a list of commands.");
+            }
+        }
+    }
 }
